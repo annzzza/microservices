b4fd8a8ca024ce9a2b4376005c2e3ca8fdbcabfe
annzzza <annacazeneuve@gmail.com>
2024-12-07 12:44:02 +0100

    pom and resttemplate updates for discovery

--- requestManagementMS/src/main/java/fr/insa/soa/requestManagementMS/RequestManagementMsApplication.java
+++ requestManagementMS/src/main/java/fr/insa/soa/requestManagementMS/RequestManagementMsApplication.java
@@ -2,9 +2,18 @@ package fr.insa.soa.requestManagementMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate,
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 
 @SpringBootApplication
 public class RequestManagementMsApplication {
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RequestManagementMsApplication.class, args);
