8d083ee47936d48b5ba29eb5ab0e1648106516cd
annzzza <annacazeneuve@gmail.com>
2024-12-20 15:41:35 +0100

    added config client with api

---
+++ ConfigClient/src/test/java/fr/insa/soa/config/ConfigClient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.soa.config.ConfigClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
